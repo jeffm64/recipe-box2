d887089782d67dbdfd561c2c86bd26fc7884e1d8
Jeff <jeff_m_england@yahoo.com>
2017-04-06 14:10:22 +0000

    Setup before attempting change around of recipe box

--- src/components/recipe-box/recipe-box.js
+++ src/components/recipe-box/recipe-box.js
@@ -1,16 +1,23 @@
 var React = require("react");
 var jQuery = require("jquery");
 
+/*
+Needs to take the input creates from add-recipe popup and rerender the recipes so as to include the newly
+added one. In the main app, when rendering RecipeBox. THAT needs to use the .map method so as to create
+multiple recipe boxes. EX: {this.state.list.map((item, key) => <Recipe name={item.name} key={key}/>)}
+*/
+
 
 var RecipeBox = React.createClass({
   
     NumberList: function() {
-      var recipeItems = this.props.recipe[0].ingredients.map(function(ingredient) {
+        var recipeItems = this.props.recipe[0].ingredients.map(function(ingredient) {
         return <li>{ingredient}</li>;
       });
         return (
           <ul>{recipeItems}</ul>
         );
+      
     },
   
     render: function() {
